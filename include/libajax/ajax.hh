8ade90467e704380d1be641628935d078eb6cdb7
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-29 09:01:14 +0200

    add data and headers to response

--- include/libajax/ajax.hh
+++ include/libajax/ajax.hh
@@ -3,10 +3,13 @@
 
 #include <libajax/request.hh>
 #include <libajax/response.hh>
+#include <cstddef>
 
 class Ajax {
     public:
         static Response perform(Request request);
+        static size_t Ajax::handleResponseHeaders(char *buffer, size_t size, size_t nitems, void *userdata);
+        static size_t Ajax::handleResponseData(char *ptr, size_t size, size_t nmemb, void *userdata);{
 };
 
 #endif
--- src/libajax/ajax.cc
+++ src/libajax/ajax.cc
@@ -15,6 +15,22 @@ void inline rescue(CURLcode code) {
     }
 }
 
+size_t Ajax::handleResponseHeaders(char *buffer, size_t size, size_t nitems, void *destination) {
+    std::map<std::string, std::string>* headers = (std::map<std::string, std::string>*) destination;
+    size_t nread = 0;
+    size_t index = 0;
+    while (nread != nitems && index != size) {
+       
+    }
+    return index;
+}
+
+size_t Ajax::handleResponseData(char *ptr, size_t size, size_t nmemb, void *destination) {
+    std::string* data = (std::string*) destination;
+    data->append(ptr, size * nmemb);
+    return size * nmmemb;
+}
+
 Response Ajax::perform(Request request) {
     CURL* handle = curl_easy_init();
     
@@ -42,16 +58,27 @@ Response Ajax::perform(Request request) {
         rescue(curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers));
         rescue(curl_easy_setopt(handle, CURLOPT_HEADEROPT, CURLHEADER_SEPARATE));
     }
+    
+    // get HEADERS
+    std::map<std::string, std::string> responseHeaders;
+    rescue(curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, &Ajax::handleResponseHeaders));
+    rescue(curl_easy_setopt(CURL *handle, CURLOPT_HEADERDATA, (void*) &responseHeaders));
+
+    // get DATA
+    std::string responseData;
+    rescue(curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, &Ajax::handleResponseData));
+    rescue(curl_easy_setopt(CURL *handle, CURLOPT_WRITEDATA, (void*) &responseData));
 
     // perform REQUEST
     rescue(curl_easy_perform(handle));
 
-    Status status;
-    rescue(curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &status));
-    
+    // get STATUS
+    Status responseStatus;
+    rescue(curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &responseStatus));
+
     curl_slist_free_all(headers);
     curl_easy_cleanup(handle);
 
     // deliver an empty response for now, TODO
-    return Response(status, "", {});
+    return Response(responseStatus, responseData, responseHeaders);
 }
